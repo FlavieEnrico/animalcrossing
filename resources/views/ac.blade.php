ac80db50fdf0eceeb676a1e734ae36cc8e474497
FlavieEnrico <72607097+FlavieEnrico@users.noreply.github.com>
2021-03-26 11:13:05 +0100

    ajout texte de préesentation

--- resources/views/ac.blade.php
+++ resources/views/ac.blade.php
@@ -19,6 +19,7 @@ $month = date("F");
 </div>
 <body>
 <div>
+<p>Welcome to the Animal Crossing: New Horizons Critterpedia! Here you'll be able to find all of the informations you need about any critter in the game. Moreover, on this welcome page, you'll find the critters that are available on your island right now.</p>
 <h2>Critters of the month: <em><?php echo $month; ?></em></h2>
 <span class="switch">
         <p>Northern hemisphere</p>
